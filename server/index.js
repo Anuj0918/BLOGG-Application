5ed85e8e0f6eacf6ee789c52dfac634379899e20
Anuj <anujtiwari2822@gmail.com>
2024-06-09 10:55:57 +0530

    changes made

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
-
-app.get('/test',(req,res) => {
-    res.json('test ok');
-})
+app.use(cors);
+app.use(express.json());
+app.post('/register',async (req,res) => {
+    const {username,password} = req.body;
+    res.json({requestData:{username,password}});
+});
 
 app.listen(4000);
